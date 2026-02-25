f49f160f3503d9adf7fb3c9004355b68688a728e
SheshuNalla <sheshunalla002@gmail.com>
2026-02-25 13:55:39 +0530

    Conditional Signout

---
+++ 02Basics/07ConditionalSignout.js
@@ -0,0 +1,14 @@
+// Show user a signout button if he is Authenticated
+// otherwise show him option to Login/Signup
+
+
+var authenticated = false;
+
+// if(authenticated){
+//     console.log("Show Signout button");
+// }else{
+//     console.log("Show login option");
+// }
+
+
+authenticated ? console.log("SignOut button") : console.log("LOGIN")

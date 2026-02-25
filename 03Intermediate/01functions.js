4c935d13f0f53da02fd3d03861d832402c528c2a
SheshuNalla <sheshunalla002@gmail.com>
2026-02-25 19:22:56 +0530

    Intermediate JS

---
+++ 03Intermediate/01functions.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log("Hello Sheshu");
+    console.log(`Hello ${name}, how are you..`)
+}
+
+
+// sayHello("Sheshu");
+
+function namaste(){
+    return "Greetings from India."
+}
+var greetings = namaste();
+
+console.log(greetings);
+console.log(namaste());

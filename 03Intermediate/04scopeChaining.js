32f7132a16baa38dda51e10a3b8ffc561700bd01
SheshuNalla <sheshunalla002@gmail.com>
2026-02-27 12:35:18 +0530

    scopeChanining

---
+++ 03Intermediate/04scopeChaining.js
@@ -0,0 +1,17 @@
+
+
+function sayName(){
+    // var name = "MR Sheshu";
+    // console.log("my name2 is",name);
+    sayName2();
+
+    function sayName2(){
+        //var name = "MR SN";
+        console.log("my name3 is",name)
+    }
+}
+var name = "Sheshu";
+
+console.log("my name1 is",name);
+
+sayName();

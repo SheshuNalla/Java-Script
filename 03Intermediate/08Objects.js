47640056b38807f883376b2134105bce2e7df492
SheshuNalla <sheshunalla002@gmail.com>
2026-03-02 12:47:49 +0530

    objectIntro

---
+++ 03Intermediate/08Objects.js
@@ -0,0 +1,22 @@
+var user = {
+    firstName : "Sheshu",
+    lastName : "Nalla",
+    role : "Admin",
+    userId : 12
+};
+
+console.log(user.firstName,user["lastName"]);
+
+// Assignment:
+
+var iPhone17 = {
+    mobileName : "Apple",
+    model : "iPhone17",
+    year : 2025,
+    features : "zoomCensor",
+    color : "orange",
+    battery : "5500maH"
+
+}
+
+console.table(iPhone17);

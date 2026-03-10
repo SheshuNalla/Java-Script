4f75c385171d8e653e82decbce7851e8706b5ea8
SheshuNalla <sheshunalla002@gmail.com>
2026-03-10 10:28:19 +0530

    revised fo loop

--- 03Intermediate/10loops.js
+++ 03Intermediate/10loops.js
@@ -9,19 +9,19 @@ let arr = [
     "Goa"
 ];
 
-// for (i = 0; i < arr.length; i++){
-//     if(typeof(arr[i]) !== "string") continue;
-//     console.log(arr[i]);
-// }
+for (let i = 0; i < arr.length; i++){
+    if(typeof(arr[i]) !== "string") continue;
+    console.log(arr[i]);
+}
 
-let i = 0;
+// let i = 0;
 
 // do {
 //     console.log(arr[i]);
 //     i++;
 // }while(i < arr.length)
 
-while(i < arr.length){
-    console.log(arr[i]);
-    i++;
-}
+// while(i < arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }

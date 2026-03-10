978132e38438d40a3f167c54ad2e6e0e163cc560
SheshuNalla <sheshunalla002@gmail.com>
2026-03-10 11:10:02 +0530

    modified with keywords

--- 03Intermediate/10loops.js
+++ 03Intermediate/10loops.js
@@ -16,14 +16,20 @@ let arr = [
 
 let i = 0; // for while and do while we have to declare i outside of the loop. 
 
-// do {
+do {
+    if(typeof arr[i] !== "number"){      // without continue/break keywords.
+        console.log(arr[i]);
+    }
+    
+    i++;
+}while(i < arr.length);
+
+// while(i < arr.length){
+//     if(typeof arr[i] !== "string"){      // with continue keyword.
+//         i++;
+//         continue;
+//     };
+    
 //     console.log(arr[i]);
 //     i++;
-// }while(i < arr.length)
-
-while(i < arr.length){
-    if(typeof arr[i] !== "number"){
-        console.log(arr[i]);
-    };
-    i++;
-}
+// }

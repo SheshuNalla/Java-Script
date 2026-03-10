4ba617ad5f6288731f61d733da1e49b6b589585e
SheshuNalla <sheshunalla002@gmail.com>
2026-03-10 12:16:31 +0530

    foreach loop

---
+++ 03Intermediate/11loopsPartTwo.js
@@ -0,0 +1,19 @@
+let myStates = [
+    "Telangana",
+    "Andhra Pradesh",
+    "Tamil Nadu",
+    1947,
+    "Rajastan",
+    "Maharastra",
+    "Uttar Pradesh",
+    "Goa"
+];
+
+// var i = 0;
+// for (;;){
+//     if(i > 3) break;
+//     console.log(i);
+//     i++;
+// }
+
+myStates.forEach((s) => (console.log(s)))

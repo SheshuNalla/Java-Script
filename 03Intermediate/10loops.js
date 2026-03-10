ed598e7da2b22721c24428196a2b57be576ce91a
SheshuNalla <sheshunalla002@gmail.com>
2026-03-10 10:55:36 +0530

    while loop

--- 03Intermediate/10loops.js
+++ 03Intermediate/10loops.js
@@ -9,19 +9,21 @@ let arr = [
     "Goa"
 ];
 
-for (let i = 0; i < arr.length; i++){
-    if(typeof(arr[i]) !== "string") continue;
-    console.log(arr[i]);
-}
+// for (let i = 0; i < arr.length; i++){
+//     if(typeof(arr[i]) !== "string") continue;
+//     console.log(arr[i]);
+// }
 
-// let i = 0;
+let i = 0; // for while and do while we have to declare i outside of the loop. 
 
 // do {
 //     console.log(arr[i]);
 //     i++;
 // }while(i < arr.length)
 
-// while(i < arr.length){
-//     console.log(arr[i]);
-//     i++;
-// }
+while(i < arr.length){
+    if(typeof arr[i] !== "number"){
+        console.log(arr[i]);
+    };
+    i++;
+}

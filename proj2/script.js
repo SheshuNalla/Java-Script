4842e3ca44508bc43cc00f845a55e2d7e27979f0
SheshuNalla <sheshunalla002@gmail.com>
2026-03-12 13:58:06 +0530

    added event listeners

--- proj2/script.js
+++ proj2/script.js
@@ -3,6 +3,7 @@ const cyan = document.querySelector(".color-cyan");
 const violet = document.querySelector(".color-violet");
 const orange = document.querySelector(".color-orange");
 const pink = document.querySelector(".color-pink");
+const body = document.querySelector(".body");
 
 
 
@@ -13,4 +14,21 @@ const getBGcolor = (selectedEl) => {
     return window.getComputedStyle(selectedEl).backgroundColor;
 }
 
-console.log(getBGcolor(violet));
+//color = getBGcolor(pink);
+
+// violet.addEventListener('mouseenter', () => {
+//     body.style.background = color;
+// })
+
+const colorChanger = (element, color) => {
+    
+    return element.addEventListener('mouseenter', () => {
+        body.style.background = color;
+    })
+}
+
+colorChanger(cyan, getBGcolor(cyan));
+colorChanger(red, getBGcolor(red));
+colorChanger(orange, getBGcolor(orange));
+colorChanger(violet, getBGcolor(violet));
+colorChanger(pink, getBGcolor(pink));

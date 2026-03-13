ca53fac4e42934b4b9e8f3fdb1053f5dc12e69bd
SheshuNalla <sheshunalla002@gmail.com>
2026-03-13 10:39:17 +0530

    creating object through constructor

---
+++ 04notAdvance/01Objects.js
@@ -0,0 +1,11 @@
+var User = function (firstName, lastName, courseCount){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.courseCount = courseCount;
+}
+
+console.log(typeof(User));
+var sheshu = new User("Sheshu", "Nalla", 4);
+
+
+// console.log(sheshu);

a16f7ef5ddf3faaf10dd85d8e6ffeb9edb90e4de
SheshuNalla <sheshunalla002@gmail.com>
2026-04-06 14:26:43 +0530

    ES6 Strings

---
+++ 05advanced/02strings.js
@@ -0,0 +1,10 @@
+var name = "sheshu";
+console.log(name);
+
+var fullName = `${name} Nalla`;    //Template literals are nothing but these "Backticks". and ${} this is known as string interpolation.
+console.log(fullName.charAt(0));
+console.log(fullName.includes("sheshi"));
+console.log(fullName.endsWith("a"));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+console.log(fullName[5]);

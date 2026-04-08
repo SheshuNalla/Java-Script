d60adfb2a1de6fdcc3aef98be63ca99e053b8a43
SheshuNalla <sheshunalla002@gmail.com>
2026-04-08 13:11:12 +0530

    private props getters and setters added

--- 05advanced/06classes.js
+++ 05advanced/06classes.js
@@ -3,19 +3,29 @@ class User{
         this.name = name;
         this.email = email;
     };
-    courseList = [];
+    #courseList = [];    //When we put a "#" hashtag before any property it becomes private and we cant access outside of the class.
 
-    enrollCourse(name){
-        this.courseList.push(name);
+    enrollCourse(name){   // In this method we are setting a value into it so it is nothing but a "Setter".
+        this.#courseList.push(name);
     }
 
-    getinfo(){
+    getinfo(){            
         return {name: this.name, email: this.email}
     }
     getCourseCount(){
-        return this.courseList.length;
+        return this.#courseList.length;
     };
+    getCourseList(){     // In this method we are getting some info and it is nothing but a "Getter".
+        return this.#courseList;
+    }
     
 };
 
 module.exports =  User;
+
+
+const rock = new User("rock", "rock@mail.com");
+
+rock.enrollCourse("java bootcamp");
+console.log(rock.getCourseList());
+console.log(rock.courseList);

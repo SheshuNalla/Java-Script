5700f55d0824afbcdabde6dd00d6b116b6486a53
SheshuNalla <sheshunalla002@gmail.com>
2026-04-16 14:52:19 +0530

    inheritance 2

--- 05advanced/08inheritance.js
+++ 05advanced/08inheritance.js
@@ -17,6 +17,7 @@ class subAdmin extends User{       // "extends" Keyword establishes the parent-c
 subAdmin.hello();  //static methods or properties can be accessed by using the class name and are useful for shared or utility logic.
 
 let sooraj = new subAdmin("sooraj", "sooraj@gmail.com", 23);
-
+sooraj.enrollCourse("python");
+console.log(sooraj.getCourseList());
 // console.log(sooraj);
 sooraj.login();

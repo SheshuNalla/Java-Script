59cebe5c204a31ff162a08057f7dc9243eaaaae0
SheshuNalla <sheshunalla002@gmail.com>
2026-04-16 14:20:41 +0530

    async and await

--- 05advanced/10promise.js
+++ 05advanced/10promise.js
@@ -9,7 +9,7 @@ const one = () => {
 // }
 
 const two = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {    //promise constructor 
         setTimeout(() => {
         resolve("Iam the Two");
     },3000)
@@ -22,11 +22,11 @@ const three = () => {
 };
 
 
-const callMe = async() => {
+const callMe = async() => {    // async keyword makes the whole method asynchronous
     valOne = one();
     console.log(valOne);
 
-    valTwo = await two();
+    valTwo = await two();     // await keyword makes js engine to wait untill the promise is settled
     console.log(valTwo);
 
     valThree = three();
